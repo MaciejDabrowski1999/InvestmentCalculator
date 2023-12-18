c1f001667663512bfa770aae56f3516594165d2d
Maciej Dąbrowski <maciej.dabrowski299@gmail.com>
2023-12-18 22:58:11 +0100

    add header component

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,25 @@
+export default function UserInput() {
+	return (
+		<div id="user-input">
+			<div className="input-group">
+				<label htmlFor="">Initial investment</label>
+				<input type="text" />
+			</div>
+			<div className="input-group">
+				<label htmlFor="">Initial investment</label>
+
+				<input type="text" />
+			</div>
+			<div className="input-group">
+				<label htmlFor="">Initial investment</label>
+
+				<input type="text" />
+			</div>
+			<div className="input-group">
+				<label htmlFor="">Initial investment</label>
+
+				<input type="text" />
+			</div>
+		</div>
+	)
+}
